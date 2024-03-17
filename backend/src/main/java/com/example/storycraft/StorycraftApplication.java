b32a011d793a30192bdf0c2ed9e367f2f61c71fb
JanB <eneroboy1@gmail.com>
2024-03-17 22:22:04 +0100

    Initial commit. (SpringBoot and React)

---
+++ backend/src/main/java/com/example/storycraft/StorycraftApplication.java
@@ -0,0 +1,13 @@
+package com.example.storycraft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorycraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorycraftApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/example/storycraft/StorycraftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.storycraft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorycraftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
