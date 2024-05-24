328716e755806be6ebebdbbb89e9ef2ad8a1e510
JanB <eneroboy1@gmail.com>
2024-05-24 12:43:09 +0200

    Added Kafka. Testing Kafka, example code.

--- backend/src/main/java/com/example/storycraft/configs/SecurityConfig.java
+++ backend/src/main/java/com/example/storycraft/configs/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig{
             "/v3/api-docs/**",
             "/swagger-ui/**",
             "/data/images/**",
+            "/kafka/send"
     };
 
     @Bean
---
+++ backend/src/main/java/com/example/storycraft/controllers/KafkaTestController.java
@@ -0,0 +1,26 @@
+package com.example.storycraft.controllers;
+
+import com.example.storycraft.kafka.KafkaProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaTestController {
+    @Autowired
+    private KafkaProducer kafkaProducer;
+
+    public KafkaTestController(KafkaProducer kafkaProducer) {
+        this.kafkaProducer = kafkaProducer;
+    }
+
+    @GetMapping("/send")
+    public String sendMessage() {
+        kafkaProducer.sendMessage("note_shared", "Hello:Kafka");
+        System.out.println("Message sent to Kafka");
+        return "Message sent to Kafka";
+    }
+
+}
